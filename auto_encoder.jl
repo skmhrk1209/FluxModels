37343101a7cecdb5c926d8bbec4d1ac5ba6bf251
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-11-18 02:57:21 +0900

    update

--- auto_encoder.jl
+++ auto_encoder.jl
@@ -7,7 +7,7 @@ train_images = Flux.Data.MNIST.images(:train)
 train_data = gpu.([(float(hcat(vec.(train_images)...)),) for train_images in Base.Iterators.partition(train_images, 100)])
 
 test_images = Flux.Data.MNIST.images(:test)
-test_data = gpu.((float(hcat(vec.(Flux.Data.MNIST.images(:test))...)),))
+test_data = gpu.((float(hcat(vec.(test_images)...)),))
 
 model = Chain(
     Dense(28 ^ 2, 64, relu),
@@ -20,12 +20,12 @@ loss(x) = Flux.mse(model(x), x)
 
 Flux.@epochs 10 Flux.train!(loss, train_data, ADAM(params(model)), cb=Flux.throttle(() -> @show(loss(test_data...)), 10))
 
-function generate()
-    image(x::Vector) = Gray.(reshape(clamp.(x, 0, 1), 28, 28))
-    inputs = rand(test_images, 10)
-    outputs = image.(map(x -> cpu(model)(float(vec(x))).data, inputs))
-    hcat(vcat.(inputs, outputs)...)
-end
+compose(f, fs...) = isempty(fs) ? (xs...) -> f(xs) : (xs...) -> compose(fs...)(f(xs))
 
-cd(@__DIR__)
-save("generated.png", generate())
+picked = rand(test_images, 10)
+generated = map(compose(
+    image -> cpu(model)(float(vec(image))), 
+    vec -> Gray.(reshape(clamp.(vec), 0, 1), 28, 28)
+), picked)
+
+save("image.png", hcat(vcat.(picked, generated)...))
